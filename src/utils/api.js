bc51e688d03b44c20197bbf36b162006e35c612f
adriantheodor <aatheodor@gmail.com>
2025-11-22 21:36:01 -0500

    fix api logic 3

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const API_BASE = import.meta.env.VITE_API_BASE;
+export const API_BASE = process.env.REACT_APP_API_BASE;
 
-console.log("API BASE:", API_BASE); // DEBUG — REMOVE LATER
+console.log("API BASE:", API_BASE);
 
 export const api = axios.create({
   baseURL: API_BASE,
