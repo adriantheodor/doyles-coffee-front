d362836af9ddb40a348ac1d8db16a97aaa11f6e5
adriantheodor <aatheodor@gmail.com>
2025-11-23 13:48:32 -0500

    fixed contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -2,7 +2,7 @@ const ContactPage = () => {
   return (
     <div>
       <h1>Contact</h1>
-      <p>Email Jeff at: <a href="mailto:jeff@example.com">jeff@example.com</a></p>
+      <p>Email Jeff at: <a href="mailto:doylesbreakroomservices@gmail.com">doylesbreakroomservices@gmail.com</a></p>
     </div>
   );
 };
