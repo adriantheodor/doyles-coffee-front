632429b951b43168ec8f24ac84579ff8ed973c3c
adriantheodor <aatheodor@gmail.com>
2025-11-24 15:30:02 -0500

    fix carousel 16

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -67,31 +67,7 @@ const HomePage = ({ onLogin }) => {
             </div>
           </div>
 
-          {/* Carousel Controls */}
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#doylesCarousel"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#doylesCarousel"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+          
         </div>
       </div>
       {/* Main Content Section */}
