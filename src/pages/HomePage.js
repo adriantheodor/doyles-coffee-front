77f8a5a57cfd83a8a5f00fdcd167d6233acf07d4
adriantheodor <aatheodor@gmail.com>
2025-11-25 15:12:00 -0500

    fix carousel 26

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -98,32 +98,6 @@ const HomePage = ({ onLogin }) => {
                     </div>
                   ))}
                 </div>
-
-                {/* Carousel Controls (Previous/Next Arrows) */}
-                <button
-                  className="carousel-control-prev"
-                  type="button"
-                  data-bs-target="#breakroomCarousel"
-                  data-bs-slide="prev"
-                >
-                  <span
-                    className="carousel-control-prev-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Previous</span>
-                </button>
-                <button
-                  className="carousel-control-next"
-                  type="button"
-                  data-bs-target="#breakroomCarousel"
-                  data-bs-slide="next"
-                >
-                  <span
-                    className="carousel-control-next-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Next</span>
-                </button>
               </div>
             </section>
 
