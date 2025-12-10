0f909be98ad0d3166bc7c243fab1f0e2822eab57
adriantheodor <aatheodor@gmail.com>
2025-12-10 12:25:25 -0500

    style 11

--- src/pages/CustomerDashPage.js
+++ src/pages/CustomerDashPage.js
@@ -3,7 +3,6 @@ import PlaceOrderPage from "./PlaceOrderPage";
 import CustomerInvoices from "./InvoicesPage";
 import CustomerIssueForm from "./SubmitIssuePage";
 import CustomerOrdersHistory from "./CustomerOrdersHistory"; 
-import "./CustomerDashPage.css";
 import { API_BASE } from "../utils/api";
 
 const CustomerDashPage = ({ activeSection, setActiveSection }) => {
